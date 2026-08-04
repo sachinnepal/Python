f46c40fada71b13e233221d9aa37e0834f941cf6
sachinnepal <sachinnepalofficial@gmail.com>
2026-08-04 21:31:46 +0545

    MODULES

--- function.py
+++ function.py
@@ -1,7 +1,21 @@
-def add_num(num1, num2=10):
+
+
+def ADD(num1, num2=10):
     sum = num1 + num2
     print(f"Sum of {num1} and {num2} is: {sum}")
 
-add_num(5,10)    
-add_num(20,30)
+def SUB(num1, num2=5):
+    diff = num1 - num2
+    print(f"Difference of {num1} and {num2} is: {diff}")    
+
+def DIV(num1, num2=2):
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+        return
+    quotient = num1 / num2
+    print(f"Quotient of {num1} and {num2} is: {quotient}")
+
+def MUL(num1, num2=3):
+    product = num1 * num2
+    print(f"Product of {num1} and {num2} is: {product}")
 
---
+++ modules.py
@@ -0,0 +1,43 @@
+from function import ADD,SUB,DIV,MUL
+
+MENU = """
+
+1. ADD
+2. SUB
+3. DIV
+4. MUL
+5. EXIT
+
+"""
+
+while True:
+    print(MENU)
+
+    choice =input("Enter your choice (1-5): ")
+
+    if choice == "1":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number (default is 10): ") or 10)
+        ADD(num1, num2)
+
+    elif choice == "2":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number (default is 5): ") or 5)
+        SUB(num1, num2)
+
+    elif choice == "3":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number (default is 2): ") or 2)
+        DIV(num1, num2)
+
+    elif choice == "4":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number (default is 3): ") or 3)
+        MUL(num1, num2)
+
+    elif choice == "5":
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
